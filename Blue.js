f15e40342f8c56cf4055914eced14ab84b8c2b99
Khalid Masum <khalid.masum.92@gmail.com>
2022-08-15 16:22:15 +0600

    Fix Red and Blue script names

--- Blue.js
+++ Blue.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         pink
+// @name         Blue
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  make Blue 
--- Red.js
+++ Red.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         pink
+// @name         Red
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  make Red 
